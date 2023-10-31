482370e7a5bcd963c13dd148acb5ea6dc0fed4ae
atichat <atichat.eng@gmail.com>
2023-10-31 18:34:14 +0700

    update dark mode in root page

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,10 @@ export default function Page() {
         <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 dark:bg-slate-800 px-6 py-10 md:w-2/5 md:px-20">
           <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
           <p
-            className={
-              lusitana.className +
-              ` text-xl text-gray-800 md:text-3xl md:leading-normal`
-            }
+            className={`${lusitana.className} text-xl text-gray-800 dark:text-slate-50 md:text-3xl md:leading-normal`}
           >
             <strong>Welcome to Acme.</strong> This is the example for the{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
@@ -38,6 +35,7 @@ export default function Page() {
             height={760}
             className="hidden md:block"
             alt="Screenshots of the dashboard project showing desktop and mobile versions"
+            priority
           />
           <Image
             src="/hero-mobile.png"
--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -16,7 +16,7 @@ export default function SideNav() {
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
-        <div className="hidden h-auto w-full grow rounded-md bg-slate-800 md:block"></div>
+        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 dark:bg-slate-800 md:block"></div>
         <form>
           <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 dark:bg-slate-800 dark:text-slate-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 dark:hover:bg-sky-100 dark:hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
